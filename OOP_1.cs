eda8cdcd422a6a0fdc8889dcf387d9086ac7a855
Volodymyr Shved <cruigma@gmail.com>
2020-11-27 18:11:04 +0200

    Add tasks 1 and 2

---
+++ OOP_1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace COOP
+{
+    internal class Rectangle
+    {
+        // Поля класса
+        private readonly double _side1;
+        private readonly double _side2;
+
+        // Конструктор
+        public Rectangle(double side1, double side2)
+        {
+            _side1 = side1;
+            _side2 = side2;
+        }
+
+        // Метод вывода
+        public void Output()
+        {
+            Console.WriteLine(Area);
+            Console.WriteLine(Perimeter);
+        }
+
+        // Свойства: площадь и периметр, только get
+        private double Area => AreaCalculator(_side1, _side2);
+        private double Perimeter => PerimeterCalculator(_side1, _side2);
+
+        // Приватные методы расчета площади и периметра
+        private static double AreaCalculator(double a, double b)
+        {
+            return a * b;
+        }
+
+        private static double PerimeterCalculator(double a, double b)
+        {
+            return 2 * (a + b);
+        }
+    }
+
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var rec = new Rectangle(Convert.ToDouble(Console.ReadLine()), Convert.ToDouble(Console.ReadLine()));
+            rec.Output();
+        }
+    }
+}
+
+/*
+    Требуется: Создать класс с именем Rectangle.
+    В теле класса создать два поля, описывающие длины сторон double side1, side2.
+    Создать пользовательский конструктор Rectangle(double side1, double side2), в теле которого
+    поля side1 и side2 инициализируются значениями аргументов.
+    Создать два метода, вычисляющие площадь прямоугольника - double AreaCalculator() и периметр
+    прямоугольника - double PerimeterCalculator().
+    Создать два свойства double Area и double Perimeter с одним методом доступа get.
+    Написать программу, которая принимает от пользователя длины двух сторон прямоугольника и выводит
+    на экран периметр и площадь.
+*/
---
+++ OOP_2.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace COOP
+{
+    internal class Book
+    {
+        // Параметрический конструктор класса
+        public Book(string title, string author, string content)
+        {
+            Title newTitle = new Title {BookTitle = title};
+            newTitle.Show(title);
+            Author newAuthor = new Author {AuthorName = author};
+            newAuthor.Show(author);
+            Content newContent = new Content{BookContent = content};
+            newContent.Show(content);
+        }
+    }
+
+    public class Title
+    {
+        public string BookTitle;
+
+        public void Show(string str)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine($"Название книги: `{str}`");
+            Console.ResetColor();
+        }
+    }
+
+    public class Author
+    {
+        public string AuthorName;
+
+        public void Show(string str)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine($"Автор: `{str}`");
+            Console.ResetColor();
+        }
+    }
+
+    public class Content
+    {
+        public string BookContent;
+
+        public void Show(string str)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Содержание книги: `{str}`");
+            Console.ResetColor();
+        }
+    }
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            new Book("Война и мир", "Лев Толстой", "Здесь будет содержание книги");
+        }
+    }
+}
+
+/*
+Создать класс Book. Создать классы Title, Author и Content,
+каждый из которых должен содержать одно строковое поле и метод void Show().
+Реализуйте возможность добавления в книгу названия книги, имени автора и содержания.
+Выведите на экран разными цветами при помощи метода Show() название книги, имя автора и
+содержание.
+*/
